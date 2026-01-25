f5141c36d4d66bb656687f2b7cf5102dc53a9453
dwlee-newword <dwlee.newword@gmail.com>
2026-01-25 13:43:02 +0900

    lint 및 build 환경 추가

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }
+      ],
+      'no-console': 'warn',
+      'no-undef': 'off',
+      eqeqeq: 'error'
+    }
+  },
+  eslintConfigPrettier
+];
